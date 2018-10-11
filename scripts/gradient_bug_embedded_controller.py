1ec993357cf68ac250e8f6fa7eb2506e9a5569be
knmcguire <k.n.mcguire@tudelft.nl>
2018-10-11 10:00:42 +0200

    [gradient_bug_em] cleaned up file

--- scripts/gradient_bug_embedded_controller.py
+++ scripts/gradient_bug_embedded_controller.py
@@ -34,20 +34,12 @@ import gradient_bug_v1
 
 class GradientBugController:
 
-    WF=wall_following.WallFollowing()
+    WF = wall_following.WallFollowing()
     RRT = receive_rostopics.RecieveROSTopic()
     GB =  gradient_bug_v1.GradientBugController()
 
     distance_to_wall = 0;
-    first_rotate = True
-    direction = 1
-    last_bearing = 0
-    hitpoint = PoseStamped()
-    stateStartTime=0
-    state = "ROTATE_TO_GOAL"
     rssi_goal_angle_adjust = 0
-    save_pos_x = []
-    save_pos_y = []
 
 
 
@@ -55,158 +47,90 @@ class GradientBugController:
 
         #Get Desired distance from the wall
         self.distance_to_wall=self.WF.getWantedDistanceToWall();
-        self.direction = self.WF.getDirectionTurn();
-        self.hitpoint = PoseStamped();
-        self.first_rotate = True;
-        self.last_bearing = 0;
-        self.stateStartTime = 0;
-        self.state =  "ROTATE_TO_GOAL"
         self.pose_tower = PoseStamped();
         self.first_run = 1
-        self.current_UWB_bearing = 2000
-        self.last_range = 0.0
-        self.current_range = 0.0
-        self.last_range_diff = 0.0
 
-        self.last_heading_rate = 2.5;
-        self.first_gradient = 1
-        self.diff_diff_range = 0.0
-        self.diff_range = 0.0
-
-        self.mem_range = 2000;
-        self.mem_heading = 1000
-        self.heading_before_turning = 0;
-
-        self.rotated_half_once = False
-        self.mem_hit_point_range = 2000
-        
         #Init embedded gradient bug
         self.GB.init(self.distance_to_wall,self.WF.getMaximumForwardSpeed(),self.WF.getMaximumRotationSpeed())
         
+        # Some values that are needed in the loop
         self.current_UWB_range = 0
         self.current_UWB_bearing = 0
-        
         self.rssi_goal_angle_adjust = 0
-        self.save_pos_x = []
-        self.save_pos_y = []
+   
 
 
 
     def stateMachine(self,RRT,odometry):
         
+        
+        # Get recieve ros topics
         self.RRT = RRT
 
-        self.current_range = self.RRT.getUWBRange()
 
-        if self.first_gradient:
-            self.last_range = self.current_range
-            self.first_gradient =0
-        self.diff_range = (self.current_range-self.last_range)/100.0
-        self.diff_diff_range = self.last_range_diff-self.diff_range;
-
-
-
-        range_front = 1000.0
-        range_side = 1000.0
-        if self.direction is 1:
-            range_front=self.RRT.getRangeFrontLeft()
-            range_side=self.RRT.getRangeLeft()
-        elif self.direction is -1:
-            range_front=self.RRT.getRangeFrontRight()
-            range_side=self.RRT.getRangeRight()
-
+        # Get bot position from odometry (with drift)
         bot_pose = PoseStamped();
         bot_pose.pose.position.x = odometry.pose.position.x;
         bot_pose.pose.position.y = odometry.pose.position.y;
-
+        
+        #If it is the first loop of the run
         if self.first_run:
-            self.bot_init_position = self.RRT.getPoseBot();
-            pose_tower_abs = self.RRT.getPoseTower();
+            self.bot_init_position = self.RRT.getPoseBot(); # Get the initial position of the bot 
+            pose_tower_abs = self.RRT.getPoseTower();   # Get the position of the tower
+            #Get the relative position to the tower (CHECK IF THIS IS STILL POSSIBLE!)
             self.pose_tower.pose.position.x = pose_tower_abs.pose.position.x - self.bot_init_position.pose.position.x;
             self.pose_tower.pose.position.y = pose_tower_abs.pose.position.y - self.bot_init_position.pose.position.y;
-            self.stateStartTime = deepcopy(self.RRT.getArgosTime())
+            # THIS MUST BE GONE IF WE ARE DOING HOMING AKA STARTING FROM TOWER!!!
             if( abs(pose_tower_abs.pose.position.x)> 0.0):
                 self.first_run = False
 
 
         else:
 
-            rel_x =  self.pose_tower.pose.position.x - bot_pose.pose.position.x  ;
-            rel_y =   self.pose_tower.pose.position.y - bot_pose.pose.position.y ;
+
+            # Get the position to the tower with the bot's odometry
+            rel_x =  self.pose_tower.pose.position.x -bot_pose.pose.position.x  
+            rel_y =   self.pose_tower.pose.position.y- bot_pose.pose.position.y 
             
-            theta = self.wrap_pi(-1*self.RRT.getHeading()+self.rssi_goal_angle_adjust);
-            #print("check theta")
-            #print(-1*self.RRT.getHeading())
-           # print(self.rssi_goal_angle_adjust)
-            #print(theta)
+            # Rotate the relative position tot tower to the bot's heading
+            # SEE IF THIS IS STILL NECESSARY!
+            theta = self.wrap_pi(-self.RRT.getHeading())
             rel_loc_x = rel_x*numpy.math.cos(theta)-rel_y*numpy.math.sin(theta)
-            rel_loc_y = rel_x*numpy.math.sin(theta)+rel_y*numpy.math.cos(theta)
+            rel_loc_y = rel_x*numpy.math.sin(theta)-rel_y*numpy.math.cos(theta)
+            
+            # Make adjusted odometry (FOR DEBUGGIN ONLY!)
+            save_pos_rel_x = 12+ bot_pose.pose.position.x - self.pose_tower.pose.position.x
+            save_pos_rel_y = 12+ bot_pose.pose.position.y - self.pose_tower.pose.position.y ;
+            adjusted_theta = self.wrap_pi(self.rssi_goal_angle_adjust)
+            rel_x_adjust = save_pos_rel_x*numpy.math.cos(adjusted_theta)-save_pos_rel_y*numpy.math.sin(adjusted_theta)
+            rel_y_adjust = save_pos_rel_x*numpy.math.sin(adjusted_theta)+save_pos_rel_y*numpy.math.cos(adjusted_theta)
+            numpy.savetxt('rel_loc_x.txt',[rel_x_adjust],delimiter=',')
+            numpy.savetxt('rel_loc_y.txt',[rel_y_adjust],delimiter=',')
+
+            # Get angle to goal and angle to goal
+            self.current_UWB_angle =  self.wrap_pi(numpy.arctan2(rel_y,rel_x))
+            self.current_UWB_range =12- math.sqrt(math.pow(rel_y,2)+math.pow(rel_x,2))
             
             
-            rel_x_adjust = bot_pose.pose.position.x*numpy.math.cos(self.rssi_goal_angle_adjust)-bot_pose.pose.position.y*numpy.math.sin(self.rssi_goal_angle_adjust)
-            rel_y_adjust = bot_pose.pose.position.x*numpy.math.sin(self.rssi_goal_angle_adjust)+bot_pose.pose.position.y*numpy.math.cos(self.rssi_goal_angle_adjust)
-
-
-            
-            self.save_pos_x.append(float(rel_x_adjust))
-            self.save_pos_y.append(float(rel_y_adjust))
-            #numpy.savetxt('rel_loc_x.txt',self.save_pos_x,delimiter=',')
-            #numpy.savetxt('rel_loc_y.txt',self.save_pos_y,delimiter=',')
-
-            #numpy.savetxt('rel_loc_x.txt',self.save_pos_x,delimiter=',')
-           # numpy.savetxt('rel_loc_y.txt',self.save_pos_y,delimiter=',')
-              #  raw_input("ARG!")
-
-
-
-
-
-
-            self.current_UWB_bearing =  self.wrap_pi(numpy.arctan2(rel_loc_y,rel_loc_x))
-            self.current_UWB_range = math.sqrt(math.pow(rel_loc_y,2)+math.pow(rel_loc_x,2))
-            
-            
-            #self.current_UWB_bearing = self.RRT.getUWBBearing();
         
-        
-        
-        
-        # Handle State transition
+        # the bearing value sometimes comes in the form or an array, just is temp fix!!
         if isinstance(self.current_UWB_bearing,numpy.ndarray):
             self.current_UWB_bearing=float(self.current_UWB_bearing[0])
             
-            
-        #print     self.RRT.getArgosTime()/10
+        #Retrieve the rssi_noise            
         rssi_noise_list =  numpy.random.normal(self.RRT.getRSSITower(),1,1);
         rssi_noise =  float(rssi_noise_list[0])
         if rssi_noise>-43:
             rssi_noise = 43 
-        #rssi_noise = self.RRT.getRSSITower()
-        
-        #print self.RRT.getArgosTime()/10
                     
+        # Call the controller from gradient_bug_v1 (gradient_bug repository)
         twist, self.rssi_goal_angle_adjust = self.GB.stateMachine(self.RRT.getRealDistanceToWall(),self.RRT.getRangeRight(),self.RRT.getRangeLeft(),
                         self.RRT.getHeading(),self.current_UWB_bearing,self.current_UWB_range, rssi_noise,self.RRT.getArgosTime()/10,False, self.WF,self.RRT)
         
-        
 
-
-
-
-       #self.cmdVelPub.publish(twist)
-        self.lastTwist = twist
-
-
-        self.last_range_diff = deepcopy(self.diff_range)
-        self.last_range = deepcopy(self.current_range)
 
         return twist
 
-
-    # Transition state and restart the timer
-    def transition(self, newState):
-        self.state = newState
-        self.stateStartTime = self.RRT.getArgosTime()
 
     # See if a value is within a margin from the wanted value
     def logicIsCloseTo(self, real_value = 0.0, checked_value =0.0, margin=0.05):
@@ -215,53 +139,6 @@ class GradientBugController:
             return True
         else:
             return False
-
-
-
-    def headingGradientDescentRange(self):
-        v = 1
-
-        command = self.last_heading_rate
-        print(self.current_range,self.diff_range,self.diff_diff_range)
-
-        if(self.diff_diff_range>0):
-            command = -1*self.last_heading_rate
-
-        print('command',command)
-
-#         if abs(self.diff_diff_range)< 0.0005:
-#             print "zero command"
-#             command = self.diff_diff_range
-
-        #command = 0
-        #w = (command + self.last_heading_rate)/2
-#         command = 1000*self.diff_diff_range
-#
-#         if abs(command)>2.5:
-#             command = 2.5 * command/abs(command)
-#
-        w = command;
-
-
-        self.last_heading_rate = deepcopy(w)
-
-        twist = Twist()
-        twist.linear.x = v
-        twist.angular.z = w
-
-        return twist
-
-    def headingZigZag(self):
-        v = 1
-        command = -1*self.last_heading_rate
-        w = command;
-        self.last_heading_rate = deepcopy(w)
-
-        twist = Twist()
-        twist.linear.x = v
-        twist.angular.z = w
-
-        return twist
 
 
     def wrap_pi(self, angles_rad):
